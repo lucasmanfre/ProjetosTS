a83e3821270a38f34e3986f35530a3016aee673d
Tonygcm <guilherme.coelhomendes@gmail.com>
2024-06-11 11:42:40 -0300

    11/06

--- BancoDeDados.cs
+++ BancoDeDados.cs
@@ -6,8 +6,9 @@ public class BancoDeDados : DbContext
     public BancoDeDados(DbContextOptions<BancoDeDados> options)
        : base(options) { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder builder) {
-        builder.UseMySQL("server=localhost;port=3306;database=projeto;user=root;password=Eldest11019!");
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.UseMySQL("server=localhost;port=3306;database=projeto;user=root;password=positivo");
     }
 
     public DbSet<Usuario> Usuarios { get; set; }
@@ -19,5 +20,5 @@ public class BancoDeDados : DbContext
     public DbSet<AvaliacaoFisica> Avaliacoes { get; set; }
 
 
-    
+
 }
